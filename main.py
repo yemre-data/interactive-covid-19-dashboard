cd9274b78a6eb328b344cacb3e1d2279a4f1457d
yemre-data <yemrecelik2525@gmail.com>
2021-05-28 12:44:31 +0200

    read new_cleaned data and transposed columns

--- main.py
+++ main.py
@@ -1,23 +1,11 @@
 import pandas as pd
-import missingno as msno
+
 
 # Read data
-df_global = pd.read_csv('Covid_Data/time_series_covid19_conf.csv')
-df_deaths = pd.read_csv('Covid_Data/time_series_covid19_deaths_global.csv')
+df_global = pd.read_csv('CovidData/data_confirmed.csv')
+df_deaths = pd.read_csv('CovidData/data_deaths.csv')
 
-# To see our data
-print(df_global.head())
-print(df_deaths.head())
 
-# See missing values
-mis_global = df_global.isnull().sum()
-mis_deaths = df_deaths.isnull().sum()
-print(mis_global)
-print(mis_deaths)
-
-# Visualisation of the missing values
-print(msno.bar(df_global,color="dodgerblue", sort="ascending", figsize=(10,5), fontsize=12))
-print(msno.bar(df_deaths,color="dodgerblue", sort="ascending", figsize=(10,5), fontsize=12))
 
 
 
