3e422076cf733057c4dc61f392c8d2756caaa6db
yemre-data <yemrecelik2525@gmail.com>
2021-05-28 14:23:11 +0200

    added libraries

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 import pandas as pd
-
+import pandas as pd
+import numpy as np
+from datetime import datetime
+import matplotlib
+import matplotlib.pyplot as plt
+from matplotlib import rcParams
+import plotly.express as px
+import plotly.graph_objects as go
+import plotly.figure_factory as ff
+from plotly.subplots import make_subplots
+import json
+from geopy.geocoders import Nominatim
+import requests
+import streamlit as st
+import folium
 
 # Read data
 df_global = pd.read_csv('CovidData/data_confirmed.csv')
 df_deaths = pd.read_csv('CovidData/data_deaths.csv')
+
 
 
 
