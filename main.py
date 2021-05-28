f64b79295fb0b931c57ed1d68ead1eb769af1716
yemre-data <yemrecelik2525@gmail.com>
2021-05-28 17:27:12 +0200

    created dashboard

--- main.py
+++ main.py
@@ -16,10 +16,26 @@ import streamlit as st
 import folium
 
 # Read data
-df_global = pd.read_csv('CovidData/data_confirmed.csv')
-df_deaths = pd.read_csv('CovidData/data_deaths.csv')
+df_global = pd.read_csv('CovidData/CONVENIENT_global_confirmed_cases.csv')
+df_deaths = pd.read_csv('CovidData/CONVENIENT_global_deaths.csv')
 
+# Some data wrangling
+df_global.dropna(axis=0,inplace=True)
+df_deaths.dropna(axis=0,inplace=True)
+df_global.rename(columns = {'Country/Region':'Date'}, inplace = True)
+df_deaths.rename(columns = {'Country/Region':'Date'}, inplace = True)
 
+# Creating streamlit Interface
+
+# setting title
+st.markdown("# Welcome to Board of Interactive COVID-19")
+st.markdown("This project has performed by the CRI team at Paris. "
+            "As vaccinations increase and covid-19 approaches towards the end. "
+            "We would like current situation of Covid on the world."
+            "The page has created for analyze Covid-19 confirmed cases and deaths data from January 2020 to now.   ")
+st.markdown("(Data has taken from open source: https://github.com/CSSEGISandData/COVID-19) ")
+
+st.image('CovidData/covid.jpg')
 
 
 
