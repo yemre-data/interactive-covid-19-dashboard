8c91863cbe89537a7518adaf6c0164cb1f266541
yemre-data <yemrecelik2525@gmail.com>
2021-06-04 15:22:43 +0200

    added new date and sorted

--- main.py
+++ main.py
@@ -12,21 +12,30 @@ from plotly.subplots import make_subplots
 import json
 from geopy.geocoders import Nominatim
 import requests
+import datetime
 import streamlit as st
 import folium
-
+import streamlit as st
+import plotly.graph_objects as go
+import numpy as np
+from plotly.subplots import make_subplots
 # Read data
-df_global = pd.read_csv('CovidData/CONVENIENT_global_confirmed_cases.csv')
-df_deaths = pd.read_csv('CovidData/CONVENIENT_global_deaths.csv')
+df_confirm = pd.read_csv('CovidData/df_confirm.csv')
+df_deaths = pd.read_csv('CovidData/df_death.csv')
+
+pd.to_datetime(df_confirm.date)
+pd.to_datetime(df_deaths.date)
+df_confirm =  df_confirm.sort_values(by=['date'], ascending = False)
+df_deaths = df_deaths.sort_values(by=['date'], ascending = False)
 
 # Some data wrangling
-df_global.dropna(axis=0,inplace=True)
-df_deaths.dropna(axis=0,inplace=True)
-df_global.rename(columns = {'Country/Region':'Date'}, inplace = True)
-df_deaths.rename(columns = {'Country/Region':'Date'}, inplace = True)
+
+country_list =list(df_confirm.country.unique())
+country_list.sort()
+#total_deaths =
+#total_confirmed =df_confirm.
 
 # Creating streamlit Interface
-
 # setting title
 st.markdown("# Welcome to Interactive Covid-19 Analyze Board ")
 st.markdown("This project has performed by the CRI team at Paris. "
@@ -37,12 +46,41 @@ st.markdown("(Data has taken from open source: https://github.com/CSSEGISandData
 
 st.image('CovidData/covid.jpg')
 st.sidebar.title("Selector")
+st.sidebar.write("-----------")
+st.sidebar.write("**Usage of Sidebar** ")
+st.sidebar.write("You can choose whether you can select confirmed cases and death cases by country and data. Feel free "
+                 "to play see the graph. ")
+
+
 case_death = st.sidebar.radio("Coivd 19 Patients Status",('Confirmed cases','Death cases'))
-country_list =list(df_global.columns.values)
-country_list.remove("Date")
-country_list = list(set(country_list))
-country_list.sort()
-select_country = st.sidebar.selectbox("Select Country",country_list)
+select_country = st.sidebar.multiselect("Select Countries",country_list)
+start_date = st.sidebar.date_input('Select Start Date', datetime.date(2020,1,23))
+end_date = st.sidebar.date_input('Select End Date', datetime.date(2021,6,4))
+st.markdown('<style>h1{color: red;}</style>', unsafe_allow_html=True)
+#st.write('<span style="color:%s">%s</span>' % ('red', " **Total Confirmed Cases : and Total Deaths : ** "), unsafe_allow_html=True)
+new_confirmed = df_confirm[df_confirm['country'].isin(select_country)]
+print(new_confirmed)
+confirmed_plot = px.line(new_confirmed, x = 'date', y = 'value',color='country')
+confirmed_plot.update_layout(title="Confirmed Cases",
+                 xaxis = dict(title = 'Date'),
+                 yaxis = dict(title = 'Number of People'),
+                 legend_title=dict(text='<b>Countries</b>'),
+                  )
+new_death =  df_deaths[df_deaths['country'].isin(select_country)]
+death_plot = px.line(new_death, x = 'date', y = 'value',color='country')
+death_plot.update_layout(title="Deaths Cases",
+                 xaxis = dict(title = 'Date'),
+                 yaxis = dict(title = 'Number of People'),
+                 legend_title=dict(text='<b>Countries</b>'),
+                  )
+if case_death  == 'Confirmed cases':
+    # st.write('<span style="color:%s">%s</span>' % ('red', " **Total Confirmed Cases : and Total Deaths : ** "), unsafe_allow_html=True)
+    st.plotly_chart(confirmed_plot,use_container_width=True)
+elif case_death == 'Death cases':
+    st.plotly_chart(death_plot, use_container_width=True)
+
+
+
 
 
 
