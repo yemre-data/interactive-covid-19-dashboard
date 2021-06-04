a7a25a756f1319d063d8ee97b0fa80899c278ec4
Kaoutar Lanjri <kaoutar.lanjri@cri-paris.org>
2021-06-04 20:02:58 +0200

    new dataset '#of_2020_population'

--- main.py
+++ main.py
@@ -25,6 +25,7 @@ df_deaths = pd.read_csv('CovidData/df_death.csv')
 
 df_confirm['date'] =pd.to_datetime(df_confirm.date)
 df_deaths['date'] = pd.to_datetime(df_deaths.date)
+
 df_confirm = df_confirm.sort_values(by=['date'], ascending = False)
 df_deaths = df_deaths.sort_values(by=['date'], ascending = False)
 
@@ -32,17 +33,18 @@ df_deaths = df_deaths.sort_values(by=['date'], ascending = False)
 
 country_list =list(df_confirm.country.unique())
 country_list.sort()
-#total_deaths =
-#total_confirmed =df_confirm.
 
 # Creating streamlit Interface
+
 # setting title
-st.markdown("# Welcome to Interactive Covid-19 Analyze Board ")
-st.markdown("This project has performed by the CRI team at Paris. "
+st.markdown("# Welcome to Interactive Covid-19 Analysis DashBoard ")
+
+st.markdown("This project is performed by the CRI team at Paris.  "
             "As vaccinations increase and covid-19 approaches towards the end, "
             "we would like analyze current situation of Covid on the world. "
-            "The page has created for analyze Covid-19 confirmed cases and deaths data from Jan 2020 to now.   ")
-st.markdown("(Data has taken from open source: https://github.com/CSSEGISandData/COVID-19) ")
+            "This page is created to analyze Covid-19 confirmed cases and deaths data from Jan 2020 to 2021.   ")
+
+st.markdown("(Data is taken from open-source: https://github.com/CSSEGISandData/COVID-19) ")
 
 st.image('CovidData/covid.jpg')
 st.sidebar.title("Selector")
@@ -60,28 +62,27 @@ st.markdown('<style>h1{color: red;}</style>', unsafe_allow_html=True)
 #st.write('<span style="color:%s">%s</span>' % ('red', " **Total Confirmed Cases : and Total Deaths : ** "), unsafe_allow_html=True)
 new_confirmed = df_confirm[df_confirm['country'].isin(select_country)]
 new_confirmed = new_confirmed.sort_values(by=['date'], ascending = False)
-confirmed_plot = px.line(new_confirmed, x = 'date', y = 'value',color='country')
-confirmed_plot.update_layout(title="Confirmed Cases",
+
+confirmed_plot = px.line(new_confirmed, x = 'date', y = 'value', color='country')
+
+confirmed_plot.update_layout(title="Cumulative Number Of Covid confirmed Cases",
                  xaxis = dict(title = 'Date'),
                  yaxis = dict(title = 'Number of People'),
                  legend_title=dict(text='<b>Countries</b>'),
                   )
+
 new_death =  df_deaths[df_deaths['country'].isin(select_country)]
 new_death = new_death.sort_values(by=['date'], ascending = False)
+
 death_plot = px.line(new_death, x = 'date', y = 'value',color='country')
-death_plot.update_layout(title="Deaths Cases",
+death_plot.update_layout(title="Cumulative Number Of Covid Death Cases",
                  xaxis = dict(title = 'Date'),
                  yaxis = dict(title = 'Number of People'),
                  legend_title=dict(text='<b>Countries</b>'),
                   )
+
 if case_death  == 'Confirmed cases':
     # st.write('<span style="color:%s">%s</span>' % ('red', " **Total Confirmed Cases : and Total Deaths : ** "), unsafe_allow_html=True)
     st.plotly_chart(confirmed_plot,use_container_width=True)
 elif case_death == 'Death cases':
     st.plotly_chart(death_plot, use_container_width=True)
-
-
-
-
-
-
