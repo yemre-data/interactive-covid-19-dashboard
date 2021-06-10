bba05fa3f101361adebe6623e3b6ea5c5a47e1d0
yemre-data <yemrecelik2525@gmail.com>
2021-06-11 00:45:36 +0200

    new data added for vacination and normalized data

--- main.py
+++ main.py
@@ -20,8 +20,15 @@ import plotly.graph_objects as go
 import numpy as np
 from plotly.subplots import make_subplots
 # Read data
+def take_data():
+    all_data = pd.read_csv('https://github.com/owid/covid-19-data/blob/master/public/data/owid-covid-data.csv?raw=true').reset_index()
+
+    all_data = all_data[['continent', 'location', 'date','new_cases','new_deaths','total_cases_per_million','total_deaths_per_million','total_vaccinations_per_hundred']].copy()
+    return all_data
+
 df_confirm = pd.read_csv('CovidData/df_confirm.csv')
 df_deaths = pd.read_csv('CovidData/df_death.csv')
+
 
 df_confirm['date'] =pd.to_datetime(df_confirm.date)
 df_deaths['date'] = pd.to_datetime(df_deaths.date)
@@ -31,7 +38,7 @@ df_deaths = df_deaths.sort_values(by=['date'], ascending = False)
 
 # Some data wrangling
 
-country_list =list(df_confirm.country.unique())
+country_list =list(all_data.location.unique())
 country_list.sort()
 
 # Creating streamlit Interface
@@ -44,22 +51,35 @@ st.markdown("This project is performed by the CRI team at Paris.  "
             "we would like analyze current situation of Covid on the world. "
             "This page is created to analyze Covid-19 confirmed cases and deaths data from Jan 2020 to 2021.   ")
 
-st.markdown("(Data is taken from open-source: https://github.com/CSSEGISandData/COVID-19) ")
+st.markdown("(Data is taken from open-source: https://github.com/owid/covid-19-data) ")
 
 st.image('CovidData/covid.jpg')
 st.sidebar.title("Selector")
 st.sidebar.write("-----------")
 st.sidebar.write("**Usage of Sidebar** ")
-st.sidebar.write("You can choose whether you can select confirmed cases and death cases by country and data. Feel free "
+st.sidebar.write(" If you want to use updated data you need to click button and it will take little time....")
+get_data =st.sidebar.button('Extract New Data')
+st.sidebar.write("You can select new cases, new_death, total confirmed cases per million("
+                 "normalized), total death cases per million(normalized), vaccination per hundred and country by "
+                 "date. Feel free "
                  "to play see the graph. ")
 
+if get_data:
+    all_data = take_data()
+else:
+    all_data = pd.read_csv('CovidData/all_data.csv')
 
-case_death = st.sidebar.radio("Coivd 19 Patients Status",('Confirmed cases','Death cases'))
-select_country = st.sidebar.multiselect("Select Countries",country_list)
-start_date = st.sidebar.date_input('Select Start Date', datetime.date(2020,1,23))
-end_date = st.sidebar.date_input('Select End Date', datetime.date(2021,6,4))
+all_data['date'] =pd.to_datetime(all_data.date)
+
+cd = st.sidebar.checkbox('New Cases and Death')
+total = st.sidebar.checkbox('Total Confirmed Cases and Deaths(normalized)')
+vac = st.sidebar.checkbox('Vaccination')
+
+select_country = st.sidebar.multiselect("Select Countries",country_list,default=["France"])
+start_date = st.sidebar.date_input('Select Start Date', datetime.date(2020,1,24))
+end_date = st.sidebar.date_input('Select End Date', datetime.datetime.now().date())
 st.markdown('<style>h1{color: red;}</style>', unsafe_allow_html=True)
-#st.write('<span style="color:%s">%s</span>' % ('red', " **Total Confirmed Cases : and Total Deaths : ** "), unsafe_allow_html=True)
+
 new_confirmed = df_confirm[df_confirm['country'].isin(select_country)]
 new_confirmed = new_confirmed.sort_values(by=['date'], ascending = False)
 
@@ -85,4 +105,5 @@ if case_death  == 'Confirmed cases':
     # st.write('<span style="color:%s">%s</span>' % ('red', " **Total Confirmed Cases : and Total Deaths : ** "), unsafe_allow_html=True)
     st.plotly_chart(confirmed_plot,use_container_width=True)
 elif case_death == 'Death cases':
+    # st.write('<span style="color:%s">%s</span>' % ('red', " **Total Confirmed Cases : and Total Deaths : ** "), unsafe_allow_html=True)
     st.plotly_chart(death_plot, use_container_width=True)
