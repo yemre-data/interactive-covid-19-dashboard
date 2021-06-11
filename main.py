7ec3b0e223a75929172366dd6acdc66076d621ac
yemre-data <yemrecelik2525@gmail.com>
2021-06-11 13:09:50 +0200

    last thing average 7 day graph and checkbox added

--- main.py
+++ main.py
@@ -52,6 +52,7 @@ st.sidebar.write('Your select will give by day results.')
 cd = st.sidebar.checkbox('New Cases and Death ')
 total = st.sidebar.checkbox('Cumulative Confirmed Cases and Deaths')
 vac = st.sidebar.checkbox('Vaccination by day and Cumulative')
+seven = st.sidebar.checkbox('Last Seven Day Average')
 
 start_date = st.sidebar.date_input('Select Start Date', datetime.date(2020, 1, 24))
 end_date = st.sidebar.date_input('Select End Date', datetime.datetime.now().date())
@@ -68,12 +69,12 @@ if cd:
     cases_death = cases_death[cases_death['location'].isin(select_country)]
     cases_plot = px.line(cases_death, x='date', y='new_cases', color='location')
     death_plot = px.line(cases_death, x='date', y='new_deaths', color='location')
-    cases_plot.update_layout(title="Number of Confirmed Cases",
+    cases_plot.update_layout(title="Number of Confirmed Cases by day",
                   xaxis=dict(title='Date'),
                   yaxis=dict(title='Number of People'),
                   legend_title=dict(text='<b>Countries</b>'),
                   )
-    death_plot.update_layout(title="Number of Confirmed Deaths",
+    death_plot.update_layout(title="Number of Confirmed Deaths by day",
                              xaxis=dict(title='Date'),
                              yaxis=dict(title='Number of People'),
                              legend_title=dict(text='<b>Countries</b>'),
@@ -109,7 +110,7 @@ if vac:
                                    legend_title=dict(text='<b>Countries</b>')
                                    )
     vac_plot = px.line(vac_df, x='date', y='new_vaccinations', color='location')
-    vac_plot.update_layout(title="Vaccinations",
+    vac_plot.update_layout(title="Vaccinations by day",
                                    xaxis=dict(title='Date'),
                                    yaxis=dict(title='Number of People per hundred'),
                                    legend_title=dict(text='<b>Countries</b>')
@@ -117,6 +118,34 @@ if vac:
     col1.plotly_chart(total_vac_plot,use_container_width=True)
     col2.plotly_chart(vac_plot,use_container_width=True)
 
+if seven:
+    yesterday = datetime.date.today() - datetime.timedelta(days=1)
+    week_ago = yesterday - datetime.timedelta(days=7)
+    date_seven = pd.date_range(week_ago, yesterday - datetime.timedelta(days=1), freq='d')
+    seven_df = all_data[all_data['date'].isin(date_seven)]
+    seven_df = seven_df[seven_df['location'].isin(select_country)]
+
+    seven_df_c = seven_df.groupby('location', as_index=False)['new_cases'].mean()
+    seven_df_d = seven_df.groupby('location', as_index=False)['new_deaths'].mean()
+    seven_df_c['new_cases'] = seven_df_c['new_cases'].fillna(0)
+    seven_df_d['new_deaths'] = seven_df_d['new_deaths'].fillna(0)
+
+    list_cases = seven_df_c['new_cases'].to_list()
+    list_death = seven_df_d['new_deaths'].to_list()
+
+    list_cases = list(map(int, list_cases))
+    list_death = list(map(int, list_death))
+    countries = seven_df['location'].to_list()
+    countries = list(set(countries))
+    number_of_country = len(countries)
+    df = pd.DataFrame(dict(country=countries * 2, average=list_death + list_cases,
+                           case_or_death=["Deaths"] * number_of_country + ["Cases"] * number_of_country))
+
+    plot_average = px.scatter(df, x="country", y="average", color="case_or_death",
+               title="Seven Days Rolling Average Deaths And Cases",
+               labels={"average": "Rolling Average"}
+               )
+    st.plotly_chart(plot_average,use_container_width=True)
 
 world_death = all_data['new_deaths'].sum(axis = 0, skipna = True)
 world_death =  '{:,.2f}'.format(world_death)
